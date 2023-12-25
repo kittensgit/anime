d26f86784085e144732716da8d7b6345a8def908
nika <nikaonime@gmail.com>
2023-12-25 21:31:32 +0200

    slow video speed

--- src/componets/video/Video.tsx
+++ src/componets/video/Video.tsx
@@ -1,12 +1,21 @@
-import { FC } from 'react';
+import { FC, useEffect, useRef } from 'react';
 
 import video from '../../assets/chainsaw.mp4';
 
 import styles from './Video.module.css';
 
 const Video: FC = () => {
+    const videoRef = useRef<HTMLVideoElement | null>(null);
+
+    useEffect(() => {
+        if (videoRef.current) {
+            videoRef.current.defaultPlaybackRate = 0.6;
+            videoRef.current.play();
+        }
+    }, []);
+
     return (
-        <video className={styles.video} autoPlay muted loop>
+        <video ref={videoRef} className={styles.video} autoPlay muted loop>
             <source src={video} type="video/mp4" />
         </video>
     );
