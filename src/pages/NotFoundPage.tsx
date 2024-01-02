b17aea398fb5d191c41f5bce9a6455c8e55b46b2
nika <nikaonime@gmail.com>
2024-01-02 11:09:43 +0200

    some styles for NotFoundPage

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,11 @@
 import { FC } from 'react';
 
 const NotFoundPage: FC = () => {
-    return <div>NotFoundPage</div>;
+    return (
+        <div className="container">
+            <h2 className="notFound">Not Found Page</h2>
+        </div>
+    );
 };
 
 export default NotFoundPage;
